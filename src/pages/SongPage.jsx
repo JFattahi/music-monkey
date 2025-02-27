c903d5304c24700c00c357aa7565068de013ac2e
olivia-onuk <oliviaonuk@gmail.com>
2025-02-27 15:42:28 -0500

    homepage and song page styles

--- src/pages/SongPage.jsx
+++ src/pages/SongPage.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./SongPage.scss";
 
 function SongPage() {
   const { id } = useParams();
 
   return (
-    <div className="song-page">
+      <div className="song-page">
       <img
         src={`http://localhost:8080/cover/${id}`}
         alt="song cover"
         className="song-cover"
       />
-      <audio controls>
+      <audio controls className="song-player">
         <source src={`http://localhost:8080/tracks/${id}`} type="audio/mpeg" />
       </audio>
     </div>
